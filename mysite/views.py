5c8a753932358eef7c6b742cf373a7671437c351
777777 <l1535414904@gmail.com>
2023-05-20 22:07:16 +0800

    更改views.py

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render   # 渲染網頁
+from django.http import HttpResponse   # Django用來回應給瀏覽器特定資料的函式
+import requests   # 匯入擷取網頁所需要的模組
+import json       # 匯入操作JSON格式所需要的模組
+from mysite import models  # 從 mysite 的資料夾中的 models.py 匯入所有的類別（資料表）
+import random     # 匯入隨機模組
 
 # Create your views here.
