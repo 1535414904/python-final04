89cca9ea033d2226e639cfe812f6aeaa2883b050
jacklin92 <127089881+jacklin92@users.noreply.github.com>
2023-05-29 08:40:11 +0800

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -4,6 +4,9 @@ import requests   # 匯入擷取網頁所需要的模組
 import json       # 匯入操作JSON格式所需要的模組
 from mysite import models  # 從 mysite 的資料夾中的 models.py 匯入所有的類別（資料表）
 import random     # 匯入隨機模組
+from datetime import datetime
+from itertools import chain
+import re 
 
 def index(request):
     mynames = ["第四組"]
